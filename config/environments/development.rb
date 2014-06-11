9f21b8eceed79dcda9b5b5329fbb3c911b49215d
rmlatz <rmlatz@gmail.com>
2014-06-11 16:13:21 -0400

    Devise Setup

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -16,6 +16,10 @@ Rails.application.configure do
   # Don't care if the mailer can't send.
   config.action_mailer.raise_delivery_errors = false
 
+  #Ensure you have defined default url options in your environments files. Here
+  # is an example of default_url_options appropriate for a development environment
+  config.action_mailer.default_url_options = { host: 'localhost:3000' }
+
   # Print deprecation notices to the Rails logger.
   config.active_support.deprecation = :log
 
