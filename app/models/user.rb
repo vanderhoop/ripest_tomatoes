52b51c3d4f7f415db9e23933eb7fc3b2c7b8ca90
rmlatz <rmlatz@gmail.com>
2014-06-12 10:08:57 -0400

    Phone Number validation

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,6 @@
 class User < ActiveRecord::Base
+  validates :phone_number, presence: true, length: {is: 10}
+  validate :phone_number_must_contain_only_numbers
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
@@ -6,4 +8,14 @@ class User < ActiveRecord::Base
 
   has_many :follows
   has_many :movies, through: :follows
+
+
+  def phone_number_must_contain_only_numbers
+    int = phone_number.to_i
+    int = int.to_s
+    if int.length != phone_number.length
+      errors.add(:phone_number, "must contain only numbers")
+    end
+  end
+
 end
